6d8c964df3382eb67befb378f4922c1d0db7f814
cnmicrobaby <microbaby@outlook.com>
2023-05-10 16:15:16 +0800

    airlinetickets 0.0.2

--- air_stations.py
+++ air_stations.py
@@ -1,6 +1,7 @@
 import re
 import requests
 
+# 返回全国城市的信息
 url = "http://webresource.c-ctrip.com/code/cquery/resource/address/flight/flight_new_poi_gb2312.js?"
 response = requests.get(url, verify=False)
 station = re.findall("([\u4e00-\u9fa5]+)\\(([A-Z]+)\\)", response.text)
